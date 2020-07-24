098a69c7e5b38d5576d4ef4290466c4b145dd5df
shanika <shanikak@hsenidmobile.com>
2020-07-24 11:41:23 +0530

    initial setup for web

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+import { createStore ,applyMiddleware} from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import logger from 'redux-logger'
+import reduxThunk from 'redux-thunk'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { BrowserRouter, Route, Link} from 'react-router-dom'
+
+const store = createStore(
+    applyMiddleware(reduxThunk, logger)
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Route path="/app" component={App}/>
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+)
